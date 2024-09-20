e1dc9649f7fc8226eb6feb8c14d932c47405edd6
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-20 23:55:52 +0600

    Gay afterlife 🖕

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -32,8 +32,56 @@ LOCAL_OVERRIDES_PACKAGES := \
     WebViewGoogle \
     WebViewGoogle-Stub \
     HealthIntelligenceStubPrebuilt
+    AmbientSensePrebuilt \
+    Aperture \
+    AmbientStreaming \
+    arcore \
+    Camera2 \
+    CarrierLocation \
+    CarrierMetrics \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DiagnosticsToolPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
+
+
+
