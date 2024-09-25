9512d0d980305f41e1ad77aa801ff0acf7b9fadf
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-25 22:14:20 +0600

    Being up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mi439.mk
+    $(LOCAL_DIR)/euclid_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mi439-user \
-    aosp_mi439-userdebug \
-    aosp_mi439-eng
+    euclid_mi439-user \
+    euclid_mi439-userdebug \
+    euclid_mi439-eng
