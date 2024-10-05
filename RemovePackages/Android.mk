16f12fdca91c960ba4ac70482342b15138a12249
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-05 11:17:13 +0600

    Ok

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -9,7 +9,30 @@ LOCAL_OVERRIDES_PACKAGES := \
     Drive \
     Gmail2 \
     Maps \
-    YouTube
+    YouTube \
+    BetterBugStub \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Chrome-Stub \
+    GoogleContacts \
+    PrebuiltDeskClockGoogle \
+    PrebuiltBugle \
+    TrichromeLibrary \
+    TrichromeLibrary-Stub \
+    GCS \
+    talkback \
+    WellbeingPrebuilt \
+    GoogleDialer \
+    DeviceIntelligenceNetworkPrebuilt \
+    DevicePersonalizationPrebuiltPixel2020 \
+    SafetyHubPrebuilt \
+    GoogleTTS \
+    TurboPrebuilt \
+    LatinIMEGooglePrebuilt \
+    WebViewGoogle \
+    WebViewGoogle-Stub \
+    HealthIntelligenceStubPrebuilt \
+    QuickSearchBox
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
