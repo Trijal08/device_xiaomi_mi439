cd43e21df4db895c749119fceb6ca602aa581300
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2024-10-13 12:44:32 -0400

    Update lineage_mi439.mk

--- lineage_mi439.mk
+++ lineage_mi439.mk
@@ -19,7 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mi439 device configuration.
 $(call inherit-product, device/xiaomi/mi439/device.mk)
 
-# Inherit from common AOSP configuration
+# Inherit some common Lineage stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_mi439
@@ -28,14 +29,63 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 
-# RisingOS stuff 
-RISING_MAINTAINER=FARHAN•AFK
-PRODUCT_NO_CAMERA := false
-ROM_FOLDER := lineage
-#WITH_GMS := true
-# Evolution-XYZ
-WITH_GMS := false 
-TARGET_HAS_UDFPS := true
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Blur
+TARGET_ENABLE_BLUR := true
+
+# BCR (Call recording)
+TARGET_PREBUILT_BCR := true
+
+# Lawnchair (Pixel launcher by default)
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Custom package installer
+TARGET_USE_CUSTOM_PACKAGE_INSTALLER := true
+
+# Live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Quick tap
+TARGET_SUPPORTS_QUICK_TAP  := true
+
+# Now Playing
+TARGET_SUPPORTS_NOW_PLAYING := true
+
+# Clear Calling
+TARGET_SUPPORTS_CLEAR_CALLING := true
+
+# Call Recording Support
+TARGET_SUPPORTS_CALL_RECORDING := true
+
+# Pixel Carrier Settings
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_PIXEL_IMS := true
+TARGET_INCLUDE_PIXEL_EUICC := true
+TARGET_INCLUDE_CARRIER_SERVICES := true
+
+# A bunch of Pixel stuff
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_DREAMLINER := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := true
+TARGET_SUPPORTS_ADPATIVE_CHARGING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_CAMERA := true
+TARGET_GOOGLE_CAMERA_LARGE_RAM := true
+TARGET_PIXEL_EXPERIENCE_2023 := true
+
+# Orion OS Flags
+ORION_MAINTAINER="GamerBoy1234294"
+ORION_MAINTAINER_LINK := "https://t.me/GamerBoy1234294"
+ORION_BUILD_TYPE := UNOFFICIAL
+ORION_GAPPS := true
+BUILD_GOOGLE_CONTACTS := true
+BUILD_GOOGLE_DIALER := true
+BUILD_GOOGLE_MESSAGE := true
 
 # Boot animation resolution.
 TARGET_BOOT_ANIMATION_RES := 720
